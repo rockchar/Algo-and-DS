27879cf82c767412edbf6ed12490627a5b056754
Rohit <rohitk.in@gmail.com>
2019-08-13 18:20:31 +0530

    structs initial commit

---
+++ Structures/Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Structures
+//
+//  Created by RockChar on 13/08/19.
+//  Copyright © 2019 RockChar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
