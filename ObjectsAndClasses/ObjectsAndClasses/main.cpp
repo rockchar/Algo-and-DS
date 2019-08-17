9979af3a062d4c40e868abbeffd01b52812f42e2
Rohit <rohitk.in@gmail.com>
2019-08-18 01:25:22 +0530

    Cleanup and notes

---
+++ ObjectsAndClasses/ObjectsAndClasses/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ObjectsAndClasses
+//
+//  Created by RockChar on 17/08/19.
+//  Copyright © 2019 RockChar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
